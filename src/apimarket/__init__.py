9824c0cfaa73d1b9d7004a9d81ad907d4cd9616f
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <24639141+sanchezcarlosjr@users.noreply.github.com>
2023-09-25 11:54:01 -0700

    Update __init__.py

--- src/apimarket/__init__.py
+++ src/apimarket/__init__.py
@@ -26,3 +26,156 @@ def fetch_curp_details(curp,api_key=False):
     }
     response = requests.post(url, headers=headers)
     return response.json()['data']
+
+
+def get_curp_from_details(nombres, paterno, materno, diaNacimiento, mesNacimiento, anoNacimiento, claveEntidad, sexo, api_key=False):
+    url = f"https://apimarket.mx/api/renapo/grupo/obtener-curp?nombres={nombres}&paterno={paterno}&materno={materno}&diaNacimiento={diaNacimiento}&mesNacimiento={mesNacimiento}&anoNacimiento={anoNacimiento}&claveEntidad={claveEntidad}&sexo={sexo}"
+    
+    headers = {
+        "Authorization": f"Bearer {config('APIMARKET_API_KEY', default=api_key)}",
+        "Accept": "application/json",
+    }
+    
+    response = requests.post(url, headers=headers)
+    return response.json()['data']
+
+
+def get_rfc_from_curp(curp, api_key=False):
+    url = f"https://apimarket.mx/api/sat/grupo/obtener-rfc?curp={curp}"
+    
+    headers = {
+        "Authorization": f"Bearer {config('APIMARKET_API_KEY', default=api_key)}",
+        "Accept": "application/json",
+    }
+    
+    response = requests.post(url, headers=headers)
+    return response.json()['data']
+
+def calculate_rfc(nombres, paterno, materno, diaNacimiento, mesNacimiento, anoNacimiento, api_key=False):
+    url = f"https://apimarket.mx/api/sat/grupo/calcular-rfc?nombres={nombres}&paterno={paterno}&materno={materno}&diaNacimiento={diaNacimiento}&mesNacimiento={mesNacimiento}&anoNacimiento={anoNacimiento}"
+    
+    headers = {
+        "Authorization": f"Bearer {config('APIMARKET_API_KEY', default=api_key)}",
+        "Accept": "application/json",
+    }
+    
+    response = requests.post(url, headers=headers)
+    return response.json()['data']
+
+
+def locate_umf_by_cp(cp, api_key=False):
+    url = f"https://apimarket.mx/api/imss/grupo/localizar-umf?cp={cp}"
+    
+    headers = {
+        "Authorization": f"Bearer {config('APIMARKET_API_KEY', default=api_key)}",
+        "Accept": "application/json",
+    }
+    
+    response = requests.post(url, headers=headers)
+    return response.json()['data']
+
+
+def locate_nss_by_curp(curp, api_key=False):
+    url = f"https://apimarket.mx/api/imss/grupo/localizar-nss?curp={curp}"
+    
+    headers = {
+        "Authorization": f"Bearer {config('APIMARKET_API_KEY', default=api_key)}",
+        "Accept": "application/json",
+    }
+    
+    response = requests.post(url, headers=headers)
+    return response.json()['data']
+
+
+def check_vigency(nss, curp, api_key=False):
+    url = f"https://apimarket.mx/api/imss/grupo/consultar-vigencia?nss={nss}&curp={curp}"
+    
+    headers = {
+        "Authorization": f"Bearer {config('APIMARKET_API_KEY', default=api_key)}",
+        "Accept": "application/json",
+    }
+    
+    response = requests.post(url, headers=headers)
+    return response.json()['data']
+
+
+def get_clinica_by_curp(curp, api_key=False):
+    url = f"https://apimarket.mx/api/imss/grupo/con-clinica?curp={curp}"
+    
+    headers = {
+        "Authorization": f"Bearer {config('APIMARKET_API_KEY', default=api_key)}",
+        "Accept": "application/json",
+    }
+    
+    response = requests.post(url, headers=headers)
+    return response.json()['data']
+
+
+def consult_clinica_by_curp(curp, api_key=False):
+    url = f"https://apimarket.mx/api/imss/grupo/con-clinica?curp={curp}"
+    
+    headers = {
+        "Authorization": f"Bearer {config('APIMARKET_API_KEY', default=api_key)}",
+        "Accept": "application/json",
+    }
+    
+    response = requests.post(url, headers=headers)
+    return response.json()['data']
+
+def get_labor_history(curp, nss, api_key=False):
+    url = f"https://apimarket.mx/api/imss/grupo/historial-laboral?curp={curp}&nss={nss}"
+    
+    headers = {
+        "Authorization": f"Bearer {config('APIMARKET_API_KEY', default=api_key)}",
+        "Accept": "application/json",
+    }
+    
+    response = requests.post(url, headers=headers)
+    return response.json()['data']
+
+
+def validate_cedula(cedula, api_key=False):
+    url = f"https://apimarket.mx/api/sep/grupo/validar-cedula?cedula={cedula}"
+    
+    headers = {
+        "Authorization": f"Bearer {config('APIMARKET_API_KEY', default=api_key)}",
+        "Accept": "application/json",
+    }
+    
+    response = requests.post(url, headers=headers)
+    return response.json()['data']
+
+
+def validate_certificate(folio, api_key=False):
+    url = f"https://apimarket.mx/api/sep/grupo/validar-certificado?folio={folio}"
+    
+    headers = {
+        "Authorization": f"Bearer {config('APIMARKET_API_KEY', default=api_key)}",
+        "Accept": "application/json",
+    }
+    
+    response = requests.post(url, headers=headers)
+    return response.json()['data']
+
+
+def obtain_cedula(nombres, paterno, materno, api_key=False):
+    url = f"https://apimarket.mx/api/sep/grupo/obtener-cedula?nombres={nombres}&paterno={paterno}&materno={materno}"
+    
+    headers = {
+        "Authorization": f"Bearer {config('APIMARKET_API_KEY', default=api_key)}",
+        "Accept": "application/json",
+    }
+    
+    response = requests.post(url, headers=headers)
+    return response.json()['data']
+
+def validate_sat_data(nombre, rfc, regimen, cp, api_key=False):
+    url = f"https://apimarket.mx/api/sat/grupo/validar-datos?nombre={nombre}&rfc={rfc}&regimen={regimen}&cp={cp}"
+    
+    headers = {
+        "Authorization": f"Bearer {config('APIMARKET_API_KEY', default=api_key)}",
+        "Accept": "application/json",
+    }
+    
+    response = requests.post(url, headers=headers)
+    return response.json()['data']
