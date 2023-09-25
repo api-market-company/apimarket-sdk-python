8e49d31e706cf220c807ee01aa5b016aaa2b4f1d
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <24639141+sanchezcarlosjr@users.noreply.github.com>
2023-09-25 13:27:40 -0700

    Update cli.py

--- src/apimarket/cli.py
+++ src/apimarket/cli.py
@@ -12,14 +12,6 @@ __license__ = "MIT"
 
 _logger = logging.getLogger(__name__)
 
-
-# ---- Python API ----
-# The functions defined in this section can be imported by users in their
-# Python scripts/interactive interpreter, e.g. via
-# `from apimarket.skeleton import fib`,
-# when using this Python module as a library.
-
-
 # ---- CLI ----
 # The functions defined in this section are wrappers around the main Python
 # API allowing them to be called directly from the terminal as a CLI
